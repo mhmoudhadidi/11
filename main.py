4fd1b3195b7d8074926310a35070b59be45f9170
mhmoudhadidi <134384059+mhmoudhadidi@users.noreply.github.com>
2023-05-23 14:13:22 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    for i in range(10):
+        basic.show_number(i)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function on_forever() {
+    for (let i = 0; i < 10; i++) {
+        basic.showNumber(i)
+    }
+})
